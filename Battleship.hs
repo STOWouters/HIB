c4a12598e5ebf6d4987d0b48f193fb6c77f7a651
Stijn-Flipper <w.stijn@gmail.com>
2014-04-07 21:04:47 +0200

    implemented board display

---
+++ Battleship.hs
@@ -0,0 +1,89 @@
+{--
+ - Battleship.hs
+ - Interactive Battleship game written in Haskell
+ -
+ - Copyright (C) 2014 Stijn Wouters
+ -
+ - This program is free software: you can redistribute it and/or modify
+ - it under the terms of the GNU General Public License as published by
+ - the Free Software Foundation, either version 3 of the License, or
+ - (at your option) any later version.
+ -
+ - This program is distributed in the hope that it will be useful,
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ - GNU General Public License for more details.
+ -
+ - You should have received a copy of the GNU General Public License
+ - along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -
+ - Last modified: 07 April 2014.
+ - By: Stijn Wouters.
+ -}
+
+-- The width of the board
+-- Because of 0-indexed, a width of 9 means 10 columns.
+width   ::  Integer
+width   =   9
+
+-- The height of the board
+-- Because of 0-indexed, a height of 9 means 10 rows.
+height  ::  Integer
+height  =   9
+
+-- Point representation
+type Point  =   (Integer, Integer)
+
+-- List of all hits and misses
+type Hits   =   [Point]
+type Misses =   [Point]
+
+-- The player's board, consisting of a set of hits and misses
+type Board  =   (Hits, Misses)
+
+-- Execute a sequence of IO operations
+io_exec         ::  [IO a] -> IO()
+io_exec []      =   return ()
+io_exec (x:xs)  =   do
+                        x;
+                        io_exec xs;
+
+-- Check whether there's a hit or a miss
+hit         ::  Point -> Hits -> Bool
+hit p hits  =   elem p hits
+
+miss        ::  Point -> Misses -> Bool
+miss p miss =   elem p miss
+
+-- Get the corresponding symbol (for outputting the board)
+symbol                  ::  Point -> Board -> Char
+symbol p board          | (hit p . fst) board   = 'x'   -- a hit!
+                        | (miss p . snd) board  = 'o'   -- a miss!
+                        | otherwise             = '-'   -- not tried that point
+
+-- Display the whole board
+display         ::  Board -> IO()
+display board   =   (io_exec . concat) [[putChar (symbol (x,y) board) | x <- [0..width]] ++ [putChar '\n'] | y <- [0..height]]
+{-
+ - This one above looks hard to understand, isn't it? So, let's break this
+ - beautiful one down for you:
+ -
+ - (1)  Use list comprehension for printing one row (on index y - just
+ -      suppose the y is already given)
+ -
+ -      `[putChar (symbol (x,y) board) | x <- [0..width]]`
+ -
+ - (2)  Put a new line after the row (so, use concatenation operator after (1))
+ -
+ -      `(1) ++ [putChar '\n']`
+ -
+ - (3)  Repeat (1) and (2) for all y coordinates (so use a list comprehension)
+ -
+ -      `[(2) | y <- [0..height]]`
+ -
+ - (4)  Since we've just generated a list of list of IO operations [ [IO a] ],
+ -      we must concatenate them so you can use the io_exec (which needs an
+ -      argument of type [IO a] and not [ [IO a] ])
+ -
+ -      `(io_exec . concat) (3)`
+ -}
