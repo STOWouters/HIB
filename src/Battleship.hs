5cf5f130e13ae341a1e5b305cbbe0e1127e149b0
Stijn-Flipper <w.stijn@gmail.com>
2014-04-11 23:59:51 +0200

    completed 'main' function

--- src/Battleship.hs
+++ src/Battleship.hs
@@ -51,23 +51,20 @@ prompt_ship who n f =   do
                             line <- getLine
                             let result = Parser.parse (Parser.points n) line
 
-                            if null result then
+                            if null result then do
                                 -- failed to parse, try again
-                                do
-                                    putStrLn "Syntax Error."
+                                putStrLn "Syntax Error."
+                                hFlush stdout
+                                prompt_ship who n f
+                            else do
+                                let ship = fst $ result!!0
+
+                                if Ship.overlap ship f then do
+                                    putStrLn "Overlaps with the fleet so far."
                                     hFlush stdout
                                     prompt_ship who n f
-                            else
-                                do
-                                    let ship = fst $ result!!0
-
-                                    if Ship.overlap ship f then
-                                        do
-                                            putStrLn "Overlaps with the fleet so far."
-                                            hFlush stdout
-                                            prompt_ship who n f
-                                    else
-                                        return ship
+                                else do
+                                    return ship
 
 -- Prompt for the coordinates.
 prompt_point        ::  String -> IO Point.Point
@@ -77,21 +74,98 @@ prompt_point who    =   do
                             line <- getLine
                             let result = Parser.parse Parser.point line
 
-                            if null result then
+                            if null result then do
                                 -- failed to parse the point, try again
-                                do
-                                    putStrLn "Syntax Error."
-                                    hFlush stdout
-                                    prompt_point who
-                            else
-                                do
-                                    let point = fst $ result!!0
-                                    return point
+                                putStrLn "Syntax Error."
+                                hFlush stdout
+                                prompt_point who
+                            else do
+                                let point = fst $ result!!0
+                                return point
 
--- The game loop, where players are shooting to each others.
-gameloop            ::  (Player.Player, Player.Player) -> IO ()
-gameloop (p1, p2)   =   do
-                            return ()
+-- The shootloop, where the player can shoot.
+shootloop           ::  (Player.Player, Player.Player) -> Int -> IO ()
+shootloop (p1,p2) 0 =   do
+                            let fleet = Player.getFleet p2
+
+                            -- If the opponent has no fleet, then stop the
+                            -- game and display both boards. Otherwise, go
+                            -- further by just switching the players.
+                            if null fleet then do
+                                let name1 = Player.getName p1
+                                let name2 = Player.getName p2
+
+                                putStrLn $ name1 ++ " wins."
+                                hFlush stdout
+
+                                putStrLn $ "Board of " ++ name1 ++ ":"
+                                hFlush stdout
+                                Board.display $ Player.getBoard p1
+
+                                putStrLn $ "Board of " ++ name2 ++ ":"
+                                hFlush stdout
+                                Board.display $ Player.getBoard p2
+                                return ()
+                            else do
+                                shootloop (p2,p1) $ length fleet
+
+shootloop (p1,p2) n =   do
+                            let fleet = Player.getFleet p2  -- fleet of opponent
+
+                            -- If the opponent has no fleet, then stop the game
+                            -- and display both boards. Otherwise, start
+                            -- shooting.
+                            if null fleet then do
+                                let name1 = Player.getName p1
+                                let name2 = Player.getName p2
+
+                                putStrLn $ name1 ++ " wins."
+                                hFlush stdout
+
+                                putStrLn $ "Board of " ++ name1 ++ ":"
+                                hFlush stdout
+                                Board.display $ Player.getBoard p1
+
+                                putStrLn $ "Board of " ++ name2 ++ ":"
+                                hFlush stdout
+                                Board.display $ Player.getBoard p2
+                                return ()
+                            else do
+                                let name = Player.getName p1    -- name of current player
+                                let board = Player.getBoard p1  -- board of current player
+
+                                -- Display current board and prompt for a
+                                -- coordinate to shoot at.
+                                Board.display board
+                                point <- prompt_point name
+
+                                if Ship.hit point fleet then do
+                                    let new_board = Board.mark point Board.Hit board
+
+                                    -- Just a hit, or sank a whole ship?
+                                    let l_before = length fleet
+                                    let new_fleet = Ship.clean . Ship.eleminate point $ fleet
+                                    let l_after = length new_fleet
+
+                                    if l_after < l_before then do
+                                        putStrLn "A ship has been sank!"
+                                        hFlush stdout
+                                    else do
+                                        putStrLn "Hit!"
+                                        hFlush stdout
+
+                                    -- update current board and opponents fleet
+                                    let new_p1 = Player.updateBoard p1 new_board
+                                    let new_p2 = Player.updateFleet p2 new_fleet
+                                    shootloop (new_p1, new_p2) $ n-1
+                                else do
+                                    putStrLn "Miss!"
+                                    hFlush stdout
+
+                                    -- update current board
+                                    let new_board = Board.mark point Board.Miss board
+                                    let new_p1 = Player.updateBoard p1 new_board
+                                    shootloop (new_p1, p2) $ n-1
 
 -- Play the game.
 play    ::  IO ()
@@ -131,5 +205,6 @@ play    =   do
                 let player1 = (name1, board, fleet1)
                 let player2 = (name2, board, fleet2)
 
-                gameloop (player1, player2)
+                -- start shootloop
+                shootloop (player1, player2) $ length fleet1
                 return ()
